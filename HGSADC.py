eb9131642125d530dd5a3448684589379d50aea4
AndreasBMoan <55883118+AndreasBMoan@users.noreply.github.com>
2020-02-11 17:07:41 +0100

    Add files via upload

--- HGSADC.py
+++ HGSADC.py
@@ -18,15 +18,15 @@ class HGSADC:
     def __init__(self, min_population_size, generation_size, max_iterations_without_improvement, time_limit):
         
         self.min_population_size = min_population_size
-        self.generation_size = 
+        self.generation_size = generation_size
         self.max_iterations_without_improvement = max_iterations_without_improvement
         self.time_limit = time_limit
         
-        self.feasible_population = #TODO
-        self.infeasible_population = #TODO
-        
         self.number_of_days_in_planning_horizon = #TODO
         self.number_of_vessels_avaliable = #TODO
+        
+        self.required_visits = {}
+        self.total_supply_jobs = type=int
         
         
         run()
@@ -36,42 +36,47 @@ class HGSADC:
     def run():
         
         t0 = time.time()
-        self.feasible_population, self.infeasible_population = initialize_population()
+        feasible_population, infeasible_population = initialize_population()
+        best_objective_value = float("inf")
         iterations_without_improvement = 0
         
         while iterations_without_improvement <= self.max_iterations_without_improvement and time.time() - t0 < self.time_limit:
             
             mom, dad = select_parents()
             
-            offspring = create_offspring(mom, dad)
+            child = create_child(mom, dad)
             
-            #Educate Child
+            child = educate(child)
             
-            #If child is infeasable
+            if child.get_feasability = False:
+                child = repair(child, probability)
                 
-                #insert into infeasible subpopulation
+            if child.get_feasability = False:
+                self.infeasible_population.append(child)
+                
+            else:
+                self.feasible_population.append(child)
+                
+            if get_population_size == min_population_size + generation_size:
+                self.infeasible_population, self.feasible_population = genecide(self.infeasible_population, self.feasible_population)
             
-                #repair with prob p
+            adjust_penalty_parameters()
             
-            #If chils is feasible
+            if child.get_objective_value < best_objective_value:
+                iterations_without_improvement = 0
+            else:
+                iterations_without_improvement += 1
+                
+            if iterations_without_improvement == iterations_before_diversification:
+                feasible_population, infeasible_population = diversify_population(feasible_population, infeasible_population)
             
-                #Insert child into feasible subpopulation
-            
-            #if maximum subpopulation size is reached
-            
-                #select survivors
-            
-            #If best solution not improved for iterations_before_diversification iterations then
-            
-                #Diversify population
-            
-            #Adjust penalty parameters for infeasability
-            
-            #if number of iterations = k * 
+        return get_best_feasible_individual(feasible_population)
             
     
     def get_population_size:
         return self.feasible_population.size() + self.infeasible_population_size()
+    
+    
     
     def is_generation_full:
         if get_population_size = self.generation_size + self.min_population_size:
@@ -81,48 +86,144 @@ class HGSADC:
         else:
             raise ValueError()
     
+    
+    
+    def get_best_feasible_individual(feasible_population):
+        #TODO!
+        pass
+    
+    
+    ############################### INITIATE POPULATION ###############################
+    
+    
+    
     def initiate_population:
         pass
+    
+    
+    ############################### SELECT PARENTS ###############################
+    
     
     def select_parents:
         mom = find_parent()
         dad = find_parent()
         return mom, dad
     
+    
+    
     def find_parent:
         candidate_1 = find_mating_candidate()
         candidate_2 = find_mating_candidate()
-        if ind.get_objective_value(candidate_1) > ind.get_objective_value(candidate_2):
+        if candidate_1.get_objective_value() > candidate_2.get_objective_value():
             return candidate_1
         else:
             return candidate_2
         
+    
+    
     def find_mating_candidate:
         size = get_population_size
         lucky_individual = random.randint(0,size)
-        if lucky_individual < self.feasible_population.size():
+        if lucky_individual < self.feasible_population.len():
             return self.feasible_population[lucky_individual]
         else:
-            return self.infeasible_population[lucky_individual - self.feasible_population.size()]
+            return self.infeasible_population[lucky_individual - self.feasible_population.len()]
         
-    def create_offspring(mom, dad):
+        
+        
+    ############################### CROSSOVER ###############################
+        
+    
+    
+    def create_child(mom type=ind, dad type=ind):
+        
+        child = type(Individual)
+        
         
         # STEP 0: INHERITANCE RULE
         
+        
         inheritance_set_1, inheritance_set_2, inheritance_set_mix = create_inheritence_rules()
+        
         
         #STEP 1: INHERIT DATA FROM MOM
         
-        #TODO!
         
-        #STEP 2: INHERIT DATA FROM DAD
+        for value_pair in inheritance_set_1:
+            
+            vessel, day = decouple_value_pair(value_pair)
+            
+            child.inherit_genes(vessel, day, mom.get_tour_gene(vessel, day))
+            
+        for value_pair in inheritance_set_mix:
+            
+            vessel, day = decouple_value_pair(value_pair)
+            
+            mom_tour_gene = mom.get_tour_gene[vessel][day]
+            length = mom_tour_gene.length()
+            
+            cutting_point_1 = random.randint(0, length)
+            cutting_point_2 = random.randint(0, length)
+            
+            if cutting_point_1 < cutting_point_2:
+                inherited_genes = mom_tour_gene[cutting_point_1:cutting_point_2-1]
+                child.inherit_genes(vessel,day,inherited_genes)
+                
+            elif cutting_point_1 > cutting_point_2:
+                inherited_genes = mom_tour_gene[:cutting_point_1-1] + mom_tour_gene[cutting_point_2:]
+                child.inherit_genes(vessel,day,inherited_genes)
+            
+            
+        #STEP 2: INHERIT DATA FROM DADDY
         
-        #TODO!
+        
+        inheritance_set_2_and_mix = inheritance_set_2 + inheritance_set_mix
+        
+        random.shuffle(inheritance_set_2_and_mix)
+        
+        for value_pair in inheritance_set_2_and_mix:
+            
+            vessel, day = decouple_value_pair(value_pair)
+            
+            for installation in dad.get_tour_gene(vessel, day):
+                
+                if day not in child.get_installation_gene(installation) and legal_pattern(child.get_installation_gene(installation), day) #TODO! Make legal pattern function
+                   
+                    if day in child.get_vessel_gene(vessel):
+                       
+                       child.inherit_tour_genes(vessel, day, installation)
+                   
+                    elif child.get_depot_capacity(day):
+                   
+                        child.inherit_tour_genes(vessel, day, installation)
+        
         
         #STEP 3: COMPLETE INSTALLATION SERVICES
         
-        #TODO!
         
+        while child.get_number_of_supply_jobs < self.total_supply_jobs:
+            
+            unsatisfied_installations = get_unsatisfied_installations()
+            
+            random.shuffle(unsatisfied_installations)
+            
+            insertions = get_feasible_insertion(unsatisfied_installations[0])
+            
+            if insertions.len() = 0:
+                
+                child = create_child(mom, dad)
+                
+            else:
+                
+                child.cheapest_insertion(insertions)
+        
+    
+    
+    def decouple_value_pair(value_pair):
+        return value_pair[0], value_pair[1]
+        
+    
+    
     def create_inheritance_rules():
         
         gene_space_size = self.number_of_days_in_planning_horizon * self.number_of_vessels_avaliable
@@ -153,20 +254,68 @@ class HGSADC:
         return gene_set_1, gene_set_2, gene_set_mix
         
         
+    
+    
     def index_to_value_pair(index):
-        vessel = math.floor(index / self.number_of_vessels_avaliable)
+        vessel = math.floor(index / self.number_of_days_in_planning_horizon)
         day = index - vessel * self.number_of_vessels_avaliable
         value_pair = [vessel, day]
         return value_pair
         
-        
-        
-        
+    
+    
+    def legal_pattern(installations, new_installation):
+        #TODO
+        pass
+    
+    
+    
+    def get_unsatisfied_installations:
+        #TODO
+        pass
+    
+    
+    
+    def get_feasible_insertion:
+        #TODO!
+        pass
+    
+    
+    
+    ############################### EDUCATION ###############################
+           
+    
+    
     def educate:
         #TODO!
         pass
     
-    def add_to_population:
+    
+    
+    ############################### DIVERSIFY POPULATION ###############################
+    
+    def diversify_population(feasible_population, infeasible_population):
+        #TODO!
+        pass
+    
+    ############################### EVALUATION ###############################
+    
+    
+    
+    def evaluate_individual(individual type=ind):
+        value = 0
+        
+        #TODO!
+        
+        return value
+    
+    
+    
+    def 
+    
+    
+    
+    def adjust_penalty_parameters:
         #TODO!
         pass
     
--- Individual.py
+++ Individual.py
@@ -5,23 +5,23 @@ Created on Tue Feb  4 11:02:58 2020
 @author: andrebmo
 """
 
+from collections import defaultdict
+
 class Individual:
     
     def __init__(self):
         
-        self.installation_chromosome = {} #Length equal to number of installations
-        self.psv_chromosome = {} #Length equal to number of psv's
+        self.installation_chromosome = defaultdict(list) #Length equal to number of installations
+        self.vessel_chromosome = defaultdict(list) #Length equal to number of vessel's
         self.tour_chromosome = {{}} #External dict determines day & vessel, internal dict determines order of installation visits
         
         self.solution{{}} #External dict determines day, vessel & intallation visit, internal dict determines speeds, cost, service time, waiting time, preparation time
         
+        self.number_of_supply_jobs = 0
+        
         self.objective_value = None #Sum of all costs including constraint relaxations
         
-        #TODO
-        
-        #Installations chromosome
-        
-        #PSV Chromosome
+        self.feasability = type=bool
         
         #For each subgroup of installation visits
         
@@ -43,7 +43,64 @@ class Individual:
                 
         #Return solution given chromosomes
         
+        def inherit_tour_genes(vessel type=int, day type=int, installations type=list):
+            self.tour_chromosome[vessel][day].append(installations)
+            self.vessel_chromosome[vessel].append(installations)
+            for installation in installations:
+                self.installation_chromosome[installation].append(day)
+            self.number_of_supply_jobs += installations.len()
+    
+        
+        
+        def get_installation_gene(installation type=int):
+            return self.installation_chromosome[installation]
+        
+        
+        
+        def get_vessel_gene(vessel type=int):
+            return self.vessel_chromosome[vessel]
+        
+        
+        
+        def get_tour_gene(vessel type=int, day type=int):
+            return self.tour_chromosome[vessel][day]
+        
+        
+        
+        def get_depot_capacity(day):
+            #TODO!
+            pass
+        
+        
+        
+        def cheapest_insertion(installation):
+            #TODO!
+            #Find the cheapest spot to insert an installation visit to the current individual
+            pass
+        
+        
+        
+        def get_number_of_supply_jobs():
+            return self.number_of_supply_jobs
+        
+       
+        
         def get_objective_value():
             return self.objective_value
+        
+        
+        
+        def educate():
+            #TODO!
+            pass
+        
+        def set_objective_value(value):
+            self.objective_value = value
+            
+            
+            
+        def get_feasability():
+            return self.feasability
+            
         
         
